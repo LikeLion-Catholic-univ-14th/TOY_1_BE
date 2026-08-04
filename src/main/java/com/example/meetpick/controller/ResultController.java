78d5268905d88c5c4681072f3222f68068e123d9
syeoooun98 <syeoooun98@catholic.ac.kr>
2026-08-04 16:25:35 +0900

    fix: ResultController 에 빠진 CrossOrigin import 추가

--- src/main/java/com/example/meetpick/controller/ResultController.java
+++ src/main/java/com/example/meetpick/controller/ResultController.java
@@ -3,6 +3,7 @@ package com.example.meetpick.controller;
 import com.example.meetpick.dto.ResultResponseDTO;
 import com.example.meetpick.service.ResultService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
