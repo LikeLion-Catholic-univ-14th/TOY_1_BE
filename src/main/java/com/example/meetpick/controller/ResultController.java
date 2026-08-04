050344bee0f7958071e50314679bb404678ff120
syeoooun98 <syeoooun98@catholic.ac.kr>
2026-08-04 15:54:05 +0900

    feat: 컨트롤러 레벨 @CrossOrigin 어노테이션 추가로 전역 CORS 보장

--- src/main/java/com/example/meetpick/controller/ResultController.java
+++ src/main/java/com/example/meetpick/controller/ResultController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/meetings")
